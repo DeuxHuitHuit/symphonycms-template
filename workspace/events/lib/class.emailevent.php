6d4392a2b2a213925b00282d3a0d85054783149d
Nicolas Brassard <nico@nitriques.com>
2012-11-01 11:10:23 -0400

    Added event libs

---
+++ workspace/events/lib/class.emailevent.php
@@ -0,0 +1,78 @@
+﻿<?php
+
+	if(!defined('__IN_SYMPHONY__')) die('<h2>Error</h2><p>You cannot directly access this file</p>');
+
+	require_once(TOOLKIT . '/class.event.php');
+
+	abstract Class EmailEvent extends Event {
+		
+		protected abstract function getSubject();
+		
+		protected abstract function getBody();
+		
+		protected abstract function getFromName();
+		
+		protected abstract function getFromEmail();
+		
+		protected abstract function getRootElement();
+		
+		protected abstract function getReceipients();
+		
+		public function load() {
+			if (isset($_POST['action']['send-email'])) {
+				return $this->__trigger();	
+			}
+			return false;
+		}
+		
+		protected function __trigger(){
+			$result = new XMLElement($this->getRootElement());
+			try {
+
+				if ($this->__sendEmail()) {
+					$result->setAttribute("result", "success");
+				} else {
+					throw new Exception('Email was not valid');
+				}
+				
+			} catch(Exception $e) {
+				$result->setAttribute("result", "error");
+				$result->appendChild(new XMLElement('error',$e->getMessage()));
+			}
+
+			return $result;
+		}
+
+		private function __sendEmail() {
+
+			$email = Email::create();
+			$email->setSenderEmailAddress($this->getReceipients());
+			$email->setFrom($this->getFromEmail(), $this->getFromName());
+			$email->setReplyToEmailAddress($this->getFromEmail());
+			$email->setRecipients($RECIPIENTS);
+			$email->setSubject($this->getSubject());
+			$email->setTextPlain($this->getBody()); 
+			
+			if ($email->validate()) {
+				$email->send();
+			} else {
+				return false;
+			}
+			
+			return true;
+		}
+		
+		protected final function getHTTPInfos() {
+			$infos =  'Referer: ' 		. General::sanitize( $_SERVER['HTTP_REFERER'] ) . PHP_EOL;
+			$infos .= 'User-Agent: '	. General::sanitize( $_SERVER['HTTP_USER_AGENT'] ) . PHP_EOL;
+			$infos .= 'IP: '			. General::sanitize( $_SERVER['REMOTE_ADDR'] ) . PHP_EOL;
+			$infos .= 'HTTP Method: '	. General::sanitize( $_SERVER['REQUEST_METHOD'] ) . PHP_EOL;
+			$infos .= 'Request URI: '	. General::sanitize( $_SERVER['REQUEST_URI'] ) . PHP_EOL;
+			$infos .= 'Request time: '	. General::sanitize( $_SERVER['REQUEST_TIME'] ) . PHP_EOL;
+			$infos .= 'CF IP: '			. General::sanitize( $_SERVER['HTTP_CF_CONNECTING_IP'] ) . PHP_EOL;
+			$infos .= 'CF Country : '	. General::sanitize( $_SERVER['HTTP_CF_IPCOUNTRY'] ) . PHP_EOL;
+			
+			return $infos;
+		}
+
+	}
---
+++ workspace/events/lib/class.insertSection.php
@@ -0,0 +1,86 @@
+<?php
+
+	if(!defined('__IN_SYMPHONY__')) die('<h2>Error</h2><p>You cannot directly access this file</p>');
+
+	require_once(TOOLKIT . '/class.event.php');
+
+	abstract class eventInsertSection extends Event {
+
+		abstract protected function getActionName();
+		abstract protected function getSection();
+		abstract protected function getFieldValue($field);
+		
+		protected function getRequestMethod() {
+			return 'POST';
+		}
+
+		public function load(){
+			return $this->__trigger();
+		}
+
+		protected function __trigger(){
+
+			$result = null;
+			$actionName = $this->getActionName();
+			$requestMethod = $this->getRequestMethod();
+			
+			if($_SERVER['REQUEST_METHOD'] == $requestMethod && isset($_POST[$actionName]) ){
+
+				$result = new XMLElement($actionName);
+				
+				$r = new XMLElement('result');
+				
+				try {
+
+					$this->createEntryFromPost();
+
+					$r->setAttribute('success', 'yes');
+
+				} catch (Exception $ex) {
+
+					$xmlEx = new XMLElement('error');
+
+					$errorMessage = Symphony::Engine()->isLoggedIn() ? $ex->getMessage() : 'A Fatal error occured';
+					
+					$xmlEx->setValue($errorMessage); 
+
+					$result->appendChild($xmlEx);
+
+					$r->setAttribute('success', 'no');
+				}
+
+				$result->appendChild($r);
+			}
+
+			return $result;
+
+		}
+
+		private function createEntryFromPost() {
+			include_once(TOOLKIT . '/class.sectionmanager.php');
+			include_once(TOOLKIT . '/class.entrymanager.php');
+
+			// section id
+			$source = $this->getSection();
+
+			$sectionManager = new SectionManager(Symphony::Engine());
+
+			$section = $sectionManager->fetch($source);
+
+			$fields = $section->fetchFields();
+
+			$entryManager = new EntryManager(Symphony::Engine());
+
+			$entry = $entryManager->create();
+			$entry->set('section_id', $source);
+
+			foreach ($fields as $f) {
+				$data = $this->getFieldValue($f->get('element_name'));
+				$entry->setData($f->get('id'), $data);
+			}
+
+			if(!$entry->commit()) {
+				throw new Exception('Could not create entry');
+			}
+		} 
+	}
