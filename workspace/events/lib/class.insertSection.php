bc8cd6153e5a5e5a2b649cc01551cd43bcdf532d
Nicolas Brassard <nico@nitriques.com>
2013-12-13 11:52:58 -0500

    Minor updates to insert section event

--- workspace/events/lib/class.insertSection.php
+++ workspace/events/lib/class.insertSection.php
@@ -21,65 +21,71 @@
 		public function load() {
 			return $this->__trigger();
 		}
-
+		
 		protected function __trigger() {
-
+			
 			$result = null;
 			$actionName = $this->getActionName();
 			$requestMethod = $this->getRequestMethod();
 			$requestArray = $this->getRequestArray();
 			
 			if($_SERVER['REQUEST_METHOD'] == $requestMethod && isset($requestArray[$actionName]) ){
-
+				
 				$result = new XMLElement($actionName);
 				
 				$r = new XMLElement('result');
 				
 				try {
-
+					
+					$this->validate();
 					$this->createEntryFromPost();
-
 					$r->setAttribute('success', 'yes');
-
+					
 				} catch (Exception $ex) {
-
+					
 					$xmlEx = new XMLElement('error');
-
-					$errorMessage = Symphony::Engine()->isLoggedIn() ? $ex->getMessage() : 'A Fatal error occured';
+					
+					$errorMessage = Symphony::Engine()->isLoggedIn() ? $ex->getMessage() : __('A Fatal error occured');
 					
 					$xmlEx->setValue($errorMessage); 
-
+					
 					$result->appendChild($xmlEx);
-
+					
 					$r->setAttribute('success', 'no');
 				}
-
+				
 				$result->appendChild($r);
+			} else {
+				throw new FrontendPageNotFoundException();
 			}
-
+			
 			return $result;
-
+			
 		}
-
+		
+		protected function validate() {}
+		
 		private function createEntryFromPost() {
 			include_once(TOOLKIT . '/class.sectionmanager.php');
 			include_once(TOOLKIT . '/class.entrymanager.php');
-
+			
 			// section id
 			$source = $this->getSection();
-
+			
 			$section = SectionManager::fetch($source);
-
+			
 			$fields = $section->fetchFields();
-
+			
 			$entry = EntryManager::create();
 			$entry->set('section_id', $source);
-
+			
 			foreach ($fields as $f) {
 				$data = $this->getFieldValue($f->get('element_name'));
-				$entry->setData($f->get('id'), $data);
+				if ($data != null) {
+					$entry->setData($f->get('id'), $data);
+				}
 			}
-
+			
 			if(!$entry->commit()) {
 				throw new Exception('Could not create entry');
 			}
