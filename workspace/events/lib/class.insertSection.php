8cd8fa6a063b74c5ff1a18e9ac88b226de245344
Nicolas Brassard <nico@nitriques.com>
2013-12-18 12:48:59 -0500

    Made it possible to render Exception message in the DS output even when not loggued in. This behavior requires a special Exception class

--- workspace/events/lib/class.insertSection.php
+++ workspace/events/lib/class.insertSection.php
@@ -1,11 +1,15 @@
 <?php
-
+	
 	if(!defined('__IN_SYMPHONY__')) die('<h2>Error</h2><p>You cannot directly access this file</p>');
-
+	
 	require_once(TOOLKIT . '/class.event.php');
-
+	
+	class InsertSectionException extends Exception {
+		
+	}
+	
 	abstract class eventInsertSection extends Event {
-
+		
 		abstract protected function getActionName();
 		abstract protected function getSection();
 		abstract protected function getFieldValue($field);
@@ -45,7 +49,8 @@
 					
 					$xmlEx = new XMLElement('error');
 					
-					$errorMessage = Symphony::Engine()->isLoggedIn() ? $ex->getMessage() : __('A Fatal error occured');
+					$showMsg = $ex instanceof InsertSectionException || Symphony::Engine()->isLoggedIn();
+					$errorMessage = $showMsg ? $ex->getMessage() : __('A Fatal error occured');
 					
 					$xmlEx->setValue($errorMessage); 
 					
