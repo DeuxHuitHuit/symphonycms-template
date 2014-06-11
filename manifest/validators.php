b2b0f9c4e9a2338ceaa3b4b74aaa02531a82ec4a
Nicolas Brassard <nico@nitriques.com>
2014-06-11 18:08:33 -0400

    video and audio validators

--- manifest/validators.php
+++ manifest/validators.php
@@ -13,4 +13,12 @@
 		'image-all' => '/\.(?:bmp|gif|jpe?g|png)$/i',
 		'document' => '/\.(?:docx?|pdf)$/i',
 		'document-all' => '/\.(?:docx?|pdf|rtf|txt)$/i',
+		'video-mp4' => '/\.(?:mp4|m4v)$/i',
+		'video-ogv' => '/\.(?:ogv|ogg)$/i',
+		'video-webm' => '/\.(?:webm|webmv)$/i',
+		'video-all' => '/\.(?:mp4|m4v|ogv|ogg|webm|webmv)$/i',
+		'audio-mp3' => '/\.(?:mp3)$/i',
+		'audio-oga' => '/\.(?:oga|ogg)$/i',
+		'audio-webm' => '/\.(?:webm|webma)$/i',
+		'audio-all' => '/\.(?:mp3|ogv|oga|webm|webma)$/i',
 	);
