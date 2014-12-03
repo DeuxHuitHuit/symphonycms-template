ee1f14798f3d8f3c87648320ce92b68b6ba409b5
Francois Vandal <francois.vandal@gmail.com>
2014-12-03 16:52:42 -0500

    Update on js global so that jit always force even number

--- workspace/assets/js/utils/global.js
+++ workspace/assets/js/utils/global.js
@@ -9,6 +9,7 @@
 	$.jitImage.defaults.forceCssResize = false;
 	$.jitImage.defaults.parallelLoadingLimit = 3;
 	$.jitImage.defaults.nonVisibleDelay = 0;
+	$.jitImage.defaults.forceEvenSize = true;
 	
 	global.raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||  
 		window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
@@ -21,20 +22,20 @@
 	// polyfill jQuery animation engine
 	if (!!$.fn.velocity) {
 		$.fn.animate = $.fn.velocity;
-		$.fn.fadeTo = function (duration, opacity) {
-			return this.velocity({opacity: opacity}, { duration: duration });
+		$.fn.fadeTo = function (duration, opacity, complete) {
+			return this.velocity({opacity: opacity}, { duration: duration, complete: complete });
 		};
-		$.fn.fadeIn = function (duration) {
-			return this.velocity('fadeIn', { duration: duration });
+		$.fn.fadeIn = function (duration, complete) {
+			return this.velocity('fadeIn', { duration: duration, complete: complete });
 		};
-		$.fn.fadeOut = function (duration) {
-			return this.velocity('fadeOut', { duration: duration });
+		$.fn.fadeOut = function (duration, complete) {
+			return this.velocity('fadeOut', { duration: duration, complete: complete });
 		};
-		$.fn.slideUp = function (duration) {
-			return this.velocity('slideUp', { duration: duration });
+		$.fn.slideUp = function (duration, complete) {
+			return this.velocity('slideUp', { duration: duration, complete: complete });
 		};
-		$.fn.slideDown = function (duration) {
-			return this.velocity('slideDown', { duration: duration });
+		$.fn.slideDown = function (duration, complete) {
+			return this.velocity('slideDown', { duration: duration, complete: complete });
 		};
 	}
 	
