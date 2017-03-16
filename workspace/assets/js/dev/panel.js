26393b9fda44ef8425a05d5edd07f31c8f6c72f8
Pascal Piché <PascalPiche@users.noreply.github.com>
2017-03-16 11:43:11 -0400

    Keep UI in sync with ajax loading of content (#153)
    Added a dev-modules to keep UI in sync with the checked option in the dev panel.
    from: d45b9bc

--- workspace/assets/js/dev/panel.js
+++ workspace/assets/js/dev/panel.js
@@ -7,17 +7,65 @@
 (function ($, global, undefined) {
 	
 	var cssClasses = [
-		'js-dev-panel',
+		'dev-js-panel',
 		'fixed fill',
 		'z-index-max',
 		'bg-color-true-white color-dark',
 		'flexbox flex-center'
 	].join(' ');
 	
+	var panel = $('<div />').attr('class', cssClasses);
+
+	var getImportantNodes = function () {
+		return 'h1, h2, h3, h4, h5, h6, footer, header, section, article';
+	};
+
+	var getJsNodes = function (prefix) {
+		return '[class^="' + prefix + '"], [class*=" ' + prefix +'"]';
+	};
+
+	var getNodeName = function (prefix, t) {
+		return t[0].nodeName.toLowerCase();
+	};
+
+	var getClassList = function (prefix, t) {
+		var pattern = new RegExp('^' + prefix);
+		var classList = t.attr('class').split(' ');
+		return _.filter(classList, function (clas) {
+			return pattern.test(clas);
+		}).join(' ');
+	};
+
+	var specialClassesFactory = function (params) {
+		var prefix = params.prefix;
+		var getValue = params.getValue;
+		var getSelector = params.getSelector;
+
+		return function (state) {
+			var fx = !state ? 'removeAttr' : 'attr';
+			$(getSelector(prefix)).each(function () {
+				var t = $(this);
+				t[fx]('data-' + prefix + 'class', getValue(prefix, t));
+			});
+		};
+	};
+
+	var specialCases = {
+		'show-js-classes': specialClassesFactory({
+			prefix: 'js-',
+			getValue: getClassList,
+			getSelector: getJsNodes
+		}),
+		'show-dom': specialClassesFactory({
+			prefix: 'dom-',
+			getValue: getNodeName,
+			getSelector: getImportantNodes
+		})
+	};
+
 	var initDevPanel = function () {
 		var storage = global.AppStorage.session;
 		var body = $('body');
-		var panel = $('<div />').attr('class', cssClasses);
 		var inner = $('<div />');
 		var devClasses = [
 			'show-classes-on-hover',
@@ -30,52 +78,15 @@
 			'show-dom',
 			'debug'
 		];
-		var getClassList = function (prefix, t) {
-			var pattern = new RegExp('^' + prefix);
-			var classList = t.attr('class').split(' ');
-			return _.filter(classList, function (clas) {
-				return pattern.test(clas);
-			}).join(' ');
-		};
-		var getNodeName = function (prefix, t) {
-			return t[0].nodeName.toLowerCase();
-		};
-		var getImportantNodes = function () {
-			return 'h1, h2, h3, h4, h5, h6, footer, header, section, article';
-		};
-		var getJsNodes = function (prefix) {
-			return '[class^="' + prefix + '"], [class*=" ' + prefix +'"]';
-		};
-		var specialClassesFactory = function (params) {
-			var prefix = params.prefix;
-			var getValue = params.getValue;
-			var getSelector = params.getSelector;
-			return function (state) {
-				var fx = !state ? 'removeAttr' : 'attr';
-				$(getSelector(prefix)).each(function () {
-					var t = $(this);
-					t[fx]('data-' + prefix + 'class', getValue(prefix, t));
-				});
-			};
-		};
-		var specialCases = {
-			'show-js-classes': specialClassesFactory({
-				prefix: 'js-',
-				getValue: getClassList,
-				getSelector: getJsNodes
-			}),
-			'show-dom': specialClassesFactory({
-				prefix: 'dom-',
-				getValue: getNodeName,
-				getSelector: getImportantNodes
-			})
-		};
+
 		var checkboxes = _.map(devClasses, function (clas) {
 			var storeKey = '-dev-' + clas;
 			var state = !!storage.get(storeKey);
 			var wrap = $('<div />').attr('class', 'margin-bottom-micro');
 			var label = $('<label />');
 			var checkbox = $('<input />').attr('type', 'checkbox').attr('class', 'margin-right-micro');
+			checkbox.addClass('dev-js-chk-' + clas);
+
 			wrap.append(label.append(checkbox).append(clas));
 			checkbox.prop('checked', state);
 			checkbox.on('change', function () {
@@ -83,6 +94,7 @@
 				var classFx = isChecked ? 'addClass' : 'removeClass';
 				storage.set(storeKey, isChecked ? '1' : '');
 				body[classFx](clas);
+
 				if (!!specialCases[clas]) {
 					specialCases[clas](isChecked);
 				}
@@ -90,16 +102,19 @@
 			if (!!state) {
 				body.addClass(clas);
 			}
+
 			if (!!specialCases[clas]) {
 				specialCases[clas](state);
 			}
 			return wrap;
 		});
+
 		panel.append(inner);
 		_.each(checkboxes, function (c) {
 			inner.append(c);
 		});
 		var attached = false;
+
 		$(document).on('keydown', function (e) {
 			if (e.which === global.keys.escape) {
 				var attFx = attached ? 'detach' : 'appendTo';
@@ -108,7 +123,39 @@
 			}
 		});
 	};
-	
+
 	$(initDevPanel);
+
+	//Add custom module to keep ui in sync with the framework state
+	if (App && App.modules && App.modules.exports) {
+
+		var refreshSpecialCases = function () {
+			//
+			if(panel.find('.dev-js-chk-show-dom:checked').length) {
+				specialCases['show-dom'](true);
+			}
+
+			if(panel.find('.dev-js-chk-show-js-classes:checked').length) {
+				specialCases['show-js-classes'](true);
+			}
+		};
+
+		App.modules.exports('dev-panel', {
+			actions: function () {
+				return {
+					page: {
+						enter: refreshSpecialCases
+						
+					},
+					articleChanger: {
+						enter: refreshSpecialCases
+					},
+					infiniteScroll: {
+						pageLoaded: refreshSpecialCases
+					}
+				};
+			}
+		});
+	}
 	
 })(jQuery, window);
