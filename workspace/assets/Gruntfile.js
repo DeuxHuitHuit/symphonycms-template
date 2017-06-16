48f07ab1b1213f41e74658b08e522beeb96209ca
Nicolas Brassard <nitriques@users.noreply.github.com>
2017-06-16 16:44:07 -0400

    Only clean less files on build
    Cleaning all the bundle would delete needed css files

--- workspace/assets/Gruntfile.js
+++ workspace/assets/Gruntfile.js
@@ -178,7 +178,7 @@ module.exports = function (grunt) {
 			'css-dev'
 		]);
 		grunt.registerTask('build', [
-			'clean:bundle',
+			'clean:bundleLess',
 			'concat:lessCore',
 			'concat:lessLib',
 			'xmlbundle:site',
