faa4863bdf600a8216ee50a27011015590bd516e
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-10-24 18:10:50 -0400

    Update workspace/assets/js/dev/panel.js
    Co-Authored-By: PascalPiche <pascal@nitriques.com>

--- workspace/assets/js/dev/panel.js
+++ workspace/assets/js/dev/panel.js
@@ -165,7 +165,8 @@
 			if (e.which === global.keys.escape && attached) {
 				var attFx = attached ? 'detach' : 'appendTo';
 				panel[attFx](attached ? undefined : body);
-				attached = !attached;
+				panel.detach();
+				attached = false;
 			} else if (e.which === global.keys.pause_break && e.shiftKey) {
 				var attFx = attached ? 'detach' : 'appendTo';
 				panel[attFx](attached ? undefined : body);
