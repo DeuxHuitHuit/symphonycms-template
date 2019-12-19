d9047ac2e3fe5102fa84435519770503c15fe05b
Dominic Blain <dominic.blain@gmail.co>
2019-12-19 15:47:49 -0500

    Comment error prone line

--- workspace/data-sources/data.navigation.php
+++ workspace/data-sources/data.navigation.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(EXTENSIONS.'/page_lhandles/lib/class.datasource.MultilingualNavigation.php');
+// require_once(EXTENSIONS.'/page_lhandles/lib/class.datasource.MultilingualNavigation.php');
 
 class datasourcenavigation extends MultilingualNavigationDatasource
 {
